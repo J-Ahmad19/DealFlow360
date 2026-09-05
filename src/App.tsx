393a0afc9140951921c608d702a59dd0ab2444c9
J-Ahmad19 <jawwadahmad568@gmail.com>
2026-09-05 12:42:41 +0530

    feat: setup new react entrypoint and global styles

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Features from './components/Features';
+import Workflow from './components/Workflow';
+import Benefits from './components/Benefits';
+import Metrics from './components/Metrics';
+import Testimonials from './components/Testimonials';
+import CTA from './components/CTA';
+import Footer from './components/Footer';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-[#fafbfc]">
+      <Navbar />
+      <main>
+        <Hero />
+        <Features />
+        <Workflow />
+        <Benefits />
+        <Metrics />
+        <Testimonials />
+        <CTA />
+      </main>
+      <Footer />
+    </div>
+  );
+}
